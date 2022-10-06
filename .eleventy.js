799ad061e5912e6d87b66fc33c3d2b20801b88c0
James Steinbach <james.steinbach@auth0.com>
2022-10-06 15:54:12 -0600

    switch to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,81 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+// const markdownItHighlightJS = require('markdown-it-highlightjs')
+
+const mdOptions = {
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+};
+
+const pathToInt = path => {
+  const ints = path.match(/\d+/g);
+  const folderValue = parseInt(ints.shift(), 10);
+  const fileValue = parseInt(ints.shift(), 10);
+  const pathInt = fileValue + folderValue * 1000;
+
+  return pathInt;
+};
+
+module.exports = eleventyConfig => {
+  eleventyConfig.addCollection('slides', collection => {
+    return collection
+      .getAll()
+      .filter(({ inputPath }) => {
+        return inputPath.match(/^\.\/src\/slides/) !== null
+      })
+      .sort((a, b) => {
+        return pathToInt(a.inputPath) < pathToInt(b.inputPath) ? -1 : 1;
+      });
+  });
+
+  eleventyConfig.addFilter('data_attrs', attrs => {
+    if (!attrs) return;
+
+    if (typeof attrs !== 'object') return;
+
+    if (Object.keys(attrs).length < 1) return;
+
+    const keys = [];
+
+    Object.keys(attrs).map(key => {
+      if (key.indexOf('data-') === 0) {
+        keys.push(`${key}="${attrs[key]}"`);
+      }
+    });
+
+    if (keys.length < 1) return;
+
+    return keys.join(' ');
+  });
+
+  eleventyConfig.setLibrary('md', markdownIt(mdOptions));
+    // .use(markdownItHighlightJS));
+
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    templateFormats: ['md', 'html']
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/reveal.js/css/': 'css',
+    './node_modules/reveal.js/js/': 'js',
+    './node_modules/reveal.js/plugin/': 'plugin'
+  });
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/webfonts');
+
+  eleventyConfig.addWatchTarget('./src/_includes/theme/**/*.scss');
+
+  return {
+    templateFormats: [
+      'liquid',
+      'md',
+      'html',
+      '11ty.js'
+    ],
+    dir: {
+      input: './src'
+    }
+  };
+};
---
+++ src/slides/0 Introduction/0 Title.md
@@ -0,0 +1,2 @@
+<h1 class="mb-1">{{slides.title}}</h1>
+<h2>{{slides.description}}</h2>
